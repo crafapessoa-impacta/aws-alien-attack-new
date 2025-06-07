a583d37a65ec778ab6c4521a87c1ffcb3134dbc5
crafapessoa-impacta <carlos.pessoa@aluno.faculdadeimpacta.com.br>
2025-06-07 16:29:49 -0300

    Update cdk.ts

--- infrastructure/cdk/bin/cdk.ts
+++ infrastructure/cdk/bin/cdk.ts
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
-import cdk = require('aws-cdk-lib');
 
+import * as cdk from 'aws-cdk-lib';
 import { MainLayer } from '../lib/layer/mainLayer';
 import { NRTAProps } from '../lib/nrta';
-import { Utils } from '../lib/util/utils'
-
+import { Utils } from '../lib/util/utils';
 
 const app = new cdk.App();
+
+// Recupera o nome do ambiente
 let envname = app.node.tryGetContext('envname');
 if (!envname) {
     console.log("****************************************************");
@@ -18,45 +19,49 @@ if (!envname) {
     console.log("****************************************************");
     process.exit(1);
 }
-else envname=envname.toUpperCase();
-console.log('# Environment name:',envname);
-var initProps = new NRTAProps();
+iza as propriedades da aplicação
+const initProps = new NRTAProps();
 initProps.setApplicationName(envname);
 
-let setApplicationProperty = (propName : string, description: string) => {
-    let envproperty = app.node.tryGetContext(propName);
-    if (envproperty) {
-        console.log('# '+description+' is going to be deployed: YES');
-        initProps.addParameter(propName,true);
+// Função auxiliar para definir propriedades da aplicação com base no contexto
+const setApplicationProperty = (propName: string, description: string): void => {
+    YES`);
+        initProps.addParameter(propName, true);
     } else {
-        console.log('# '+description+' is going to be deployed: NO');
-    };
-}
+        console.log(`# ${description} is going to be deployed: NO`);
+    }
+};
 
-// Getting other possible context names
-// FOR THE CDN DEPLOYMENT
-setApplicationProperty("deploycdn","Cloudfront");
+// Define propriedades opcionais com base no contexto
+setApplicationProperty("deploycdn", "CloudFront");
+setApplicationProperty("sessionparameter", "SSM Parameter Session");
+setApplicationProperty("kinesisintegration", "Kinesis Data Streams integration");
+setApplicationProperty("firehose", "Kinesis Firehose");
 
-// Getting other possible context names
-// FOR SSM PARAMETER
-setApplicationProperty("sessionparameter","SSM Parameter Session");
-
-// Getting other possible context names
-// FOR KINESIS DATA STREAMS INTEGRATION
-setApplicationProperty("kinesisintegration","Kinesis Data Streams integration");
-
-// Getting other possible context names
-// FOR KINESIS FIREHOSE
-setApplicationProperty("firehose","Kinesis Firehose");
-
+// Lista de regiões a serem implantadas
+const regions = ['us-east-1', 'us-west-1'];
+const account = process.env.CDK_DEFAULT_ACCOUNT;
 
 Utils.checkforExistingBuckets(initProps.getBucketNames())
     .then((listOfExistingBuckets) => {
-        if (listOfExistingBuckets && listOfExistingBuckets.length > 0)
-            console.log("# The following buckets are NOT being created because they already exist: ", listOfExistingBuckets);
+        if (listOfExistingBuckets && listOfExistingBuckets.length > 0) {
+            console.log("# The following buckets are NOT being created because they already exist:", listOfExistingBuckets);
+        }
         initProps.addParameter('existingbuckets', listOfExistingBuckets);
-        new MainLayer(app, initProps.getApplicationName(), initProps);
-})
+
+        // Cria uma stack para cada região
+        regions.forEach(region => {
+            const env = { account, region };
+            const regionalProps = new NRTAProps();
+
+            // Copia os parâmetros do initProps
+            regionalProps.setApplicationName(`${envname}-${region}`);
+            regionalProps.copyFrom?.(initProps); // Se existir método copyFrom
+            regionalProps.addParameter('existingbuckets', listOfExistingBuckets);
+
+            new MainLayer(app, `${envname}-${region}`, regionalProps, { env });
+        });
+    })
     .catch((errorList) => {
         console.log(errorList);
-});
+    });
