29babdc5534a9ed8cbb4b92f5b25654f3ac29fbd
crafapessoa-impacta <carlos.pessoa@aluno.faculdadeimpacta.com.br>
2025-06-07 16:35:10 -0300

    Update cdk.ts

--- infrastructure/cdk/bin/cdk.ts
+++ infrastructure/cdk/bin/cdk.ts
@@ -1,7 +1,4 @@
 #!/usr/bin/env node
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: MIT-0
-
 import * as cdk from 'aws-cdk-lib';
 import { MainLayer } from '../lib/layer/mainLayer';
 import { NRTAProps } from '../lib/nrta';
@@ -12,24 +9,30 @@ const app = new cdk.App();
 // Recupera o nome do ambiente
 let envname = app.node.tryGetContext('envname');
 if (!envname) {
-    console.log("****************************************************");
-    console.log("ERROR: your environment name is undefined.\n");
-    console.log("Please run the command like this:");
-    console.log("cdk [synth|deploy|destroy] -c envname=<your environment name>");
-    console.log("****************************************************");
-    process.exit(1);
+  console.error("****************************************************");
+  console.error("ERROR: your environment name is undefined.\n");
+  console.error("Please run the command like this:");
+  console.error("cdk [synth|deploy|destroy] -c envname=<your environment name>");
+  console.error("****************************************************");
+  process.exit(1);
 }
-iza as propriedades da aplicação
+
+envname = envname.toUpperCase();
+console.log('# Environment name:', envname);
+
+// Inicializa as propriedades da aplicação
 const initProps = new NRTAProps();
 initProps.setApplicationName(envname);
 
 // Função auxiliar para definir propriedades da aplicação com base no contexto
 const setApplicationProperty = (propName: string, description: string): void => {
-    YES`);
-        initProps.addParameter(propName, true);
-    } else {
-        console.log(`# ${description} is going to be deployed: NO`);
-    }
+  const envProperty = app.node.tryGetContext(propName);
+  if (envProperty) {
+    console.log(`# ${description} is going to be deployed: YES`);
+    initProps.addParameter(propName, true);
+  } else {
+    console.log(`# ${description} is going to be deployed: NO`);
+  }
 };
 
 // Define propriedades opcionais com base no contexto
@@ -43,25 +46,26 @@ const regions = ['us-east-1', 'us-west-1'];
 const account = process.env.CDK_DEFAULT_ACCOUNT;
 
 Utils.checkforExistingBuckets(initProps.getBucketNames())
-    .then((listOfExistingBuckets) => {
-        if (listOfExistingBuckets && listOfExistingBuckets.length > 0) {
-            console.log("# The following buckets are NOT being created because they already exist:", listOfExistingBuckets);
-        }
-        initProps.addParameter('existingbuckets', listOfExistingBuckets);
+  .then((existingBuckets: string[]) => {
+    if (existingBuckets?.length > 0) {
+      console.log("# The following buckets are NOT being created because they already exist:", existingBuckets);
+    }
 
-        // Cria uma stack para cada região
-        regions.forEach(region => {
-            const env = { account, region };
-            const regionalProps = new NRTAProps();
+    initProps.addParameter('existingbuckets', existingBuckets);
 
-            // Copia os parâmetros do initProps
-            regionalProps.setApplicationName(`${envname}-${region}`);
-            regionalProps.copyFrom?.(initProps); // Se existir método copyFrom
-            regionalProps.addParameter('existingbuckets', listOfExistingBuckets);
+    // Cria uma stack para cada região
+    regions.forEach(region => {
+      const env: cdk.Environment = { account, region };
 
-            new MainLayer(app, `${envname}-${region}`, regionalProps, { env });
-        });
-    })
-    .catch((errorList) => {
-        console.log(errorList);
+      // Clona as propriedades para cada região
+      const regionalProps = new NRTAProps();
+      regionalProps.setApplicationName(`${envname}-${region}`);
+      regionalProps.copyFrom?.(initProps); // Se existir método copyFrom
+      regionalProps.addParameter('existingbuckets', existingBuckets);
+
+      new MainLayer(app, `${envname}-${region}`, regionalProps, { env });
     });
+  })
+  .catch((error) => {
+    console.error("Erro ao verificar buckets existentes:", error);
+  });
